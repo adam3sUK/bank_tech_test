5b34c7d837701edaba7a955d25e6bec7bba9aef1
Adam Harbisher <aharbisher@gmail.com>
2022-01-04 12:04:12 +0000

    Added passing test for deposit method

--- lib/account.rb
+++ lib/account.rb
@@ -1,10 +1,15 @@
 class Account
+  attr_reader :balance
 
-  def initalize
-    @balance = 0
+  def initialize
+    @balance = 0.00
   end
 
   def self.create
     Account.new
+  end
+
+  def deposit(amount)
+    @balance += amount.to_f
   end
 end
--- spec/account_class_spec.rb
+++ spec/account_class_spec.rb
@@ -1,10 +1,26 @@
 require 'account'
 
 describe Account do
+  before(:each) do
+    @account = Account.create
+  end
+
   describe '.create' do
     it 'creates a new Account instance' do
-      account = Account.create
-			expect(account).to be_an_instance_of(Account)
+			expect(@account).to be_an_instance_of(Account)
+    end
+  end
+
+  describe '.balance' do
+    it 'shows the user the balance of the Account, will be 0 on a new account' do
+      expect(@account.balance).to be(0.00)
+    end
+  end
+
+  describe '.deposit' do
+    it 'increases the balance by the amount deposited' do
+      @account.deposit(500)
+      expect(@account.balance).to be(500.00)
     end
   end
 end
