04437ae3b2773c62b7c43a8385fdc76ec15b9a4a
Adam Harbisher <aharbisher@gmail.com>
2022-01-05 11:23:25 +0000

    added passing test for print_header method on PrintStatement class

--- lib/print_statement.rb
+++ lib/print_statement.rb
@@ -1,4 +1,6 @@
 # Prints a record to the terminal showings the accounts transactional history
 class PrintStatement
-
+  def print_header
+    puts 'date || credit || debit || balance'
+  end
 end
--- spec/print_statement_class_spec.rb
+++ spec/print_statement_class_spec.rb
@@ -1,10 +1,19 @@
 require './lib/print_statement.rb'
 
 describe PrintStatement do
+  before(:each) do
+    @statement = PrintStatement.new
+  end
+
   describe '.new' do
     it 'creates a new PrintStatement instance' do
-      statement = PrintStatement.new
-      expect(statement).to be_an_instance_of(PrintStatement)
+      expect(@statement).to be_an_instance_of(PrintStatement)
+    end
+  end
+
+  describe '.print_header' do
+    it 'prints the header of the statement' do
+      expect{ @statement.print_header }.to output(a_string_including('date || credit || debit || balance')).to_stdout
     end
   end
 end
